89f9e3d1f8a3e1d58d1e5daeea243114fac26268
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-08-27 22:02:20 +0530

    .gitignore added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="HTMLrenderer-c17hawke",
+    name="HTMLrenderer",
     version="0.0.1",
     author="c17hawke",
     author_email="sunny.c17hawke@gmail.com",
