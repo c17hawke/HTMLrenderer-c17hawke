a8036ccdb7dbe6d04e2b1bab86bb0044464f6ffa
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-08-27 23:06:02 +0530

    testing one

---
+++ tests/test_one.py
@@ -0,0 +1,7 @@
+import IPython
+from src.HTMLrenderer.render import render_site
+
+def test_render_site(URL="https://pytorch.org"):
+    res = render_site(URL)
+    assert isinstance(res, IPython.lib.display.IFrame) == False
+
