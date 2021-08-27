c9a5933c7039fef32fd5c532cc9839722f02b3ee
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-08-27 23:12:40 +0530

    testing done

--- tests/test_one.py
+++ tests/test_one.py
@@ -4,4 +4,4 @@ from src.HTMLrenderer.render import render_site
 
 def test_render_site(URL="https://pytorch.org"):
     res = render_site(URL)
-    assert isinstance(res, IPython.lib.display.IFrame) == True
+    assert isinstance(res, IPython.lib.display.IFrame)
