ad892c915ff95696da7e4df573ba74c2d17b8534
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-08-27 22:26:46 +0530

    minor update

--- README.md
+++ README.md
@@ -3,7 +3,19 @@ A small package for rendering HTML pages in jupyter notebooks
 
 # How to use -
 
-run below snippet of code -
+* install the latest package 
+
+> * in jupyter notebook -
+```
+    !pip install HTMLrenderer
+```
+
+> * in command prompt -
+```bash    
+    pip install HTMLrenderer
+```
+
+* Now run below snippets of code in your jupyter-notebooks cell to render your website as an IFrame-
 
 ## default use case -
 ```python
--- src/HTMLrenderer/render.py
+++ src/HTMLrenderer/render.py
@@ -1,4 +1,14 @@
 from IPython.display import IFrame
 
-def render_site(URL, width="100%", height=600):
+def render_site(URL, width="100%", height="600"):
+  """Renders HTML in the jupyter notebook
+
+  Args:
+      URL (str): URL of site to render in jupyter notebook
+      width (str, optional): width of the html page to render_site. Defaults to "100%".
+      height (str, optional): height of the html page to render. Defaults to 600.
+
+  Returns:
+      IFrame: Renders HTML in the jupyter notebook
+  """
   return IFrame(src=URL, width=width, height=height)
