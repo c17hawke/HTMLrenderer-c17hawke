d14defbb82f51ff1102274ec9ee0c73a3efc55f1
c17hawke <sunny.c17hawke@gmail.com>
2022-09-04 15:10:41 +0530

    upgrade

--- setup.py
+++ setup.py
@@ -3,17 +3,29 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("src/HTMLrenderer/VERSION", "r", encoding="utf-8") as fh:
+    __version__ = fh.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "HTMLrenderer-c17hawke" # github repo name
+AUTHOR_USER_NAME = "c17hawke" # update as per your need
+SRC_REPO = "HTMLrenderer" # example sklearn etc
+LIST_OF_REQUIREMENTS = [
+        'ipython',
+        'ipywidgets'
+    ] # core dependencies
+
 setuptools.setup(
     name="HTMLrenderer",
-    version="0.0.10",
+    version=__version__,
     author="c17hawke",
     author_email="sunny.c17hawke@gmail.com",
     description="A small package for rendering HTML pages in jupyter notebooks",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/HTMLrenderer-c17hawke",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": "https://github.com/c17hawke/HTMLrenderer-c17hawke/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,9 +33,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(where="src", exclude="tests"),
     python_requires=">=3.6",
-    install_requires=[
-        'ipython',
-    ],
+    install_requires=LIST_OF_REQUIREMENTS
 )
