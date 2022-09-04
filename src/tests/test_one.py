8fc8a85a19ced8f2b44c1e22db83266cf45b06ad
c17hawke <sunny.c17hawke@gmail.com>
2022-09-04 22:12:43 +0530

    test case success

--- src/tests/test_one.py
+++ src/tests/test_one.py
@@ -1,11 +1,27 @@
-import IPython
-from src.HTMLrenderer.render import render_site
+import pytest
+from HTMLrenderer import render_site
+from HTMLrenderer.custom_exception import InvalidURLException
 
 
-# def test_render_site(URL="https://pytorch.org"):
-#     res = render_site(URL)
-#     assert isinstance(res, IPython.lib.display.IFrame)
+URL_test_success_data = [
+    ("http://pytorch.org", "success"),
+    ("https://pytorch.org", "success"),
+]
 
-def test_render_site(URL="https://pytorch.org"):
-    res = render_site(URL)
-    assert res == None
+URL_test_bad_data = [
+    ("http://pytorch"),
+    ("http//pytorch"),
+    ("http:/pytorch"),
+    ("http/pytorch"),
+    ("http/pytorch"),
+    ("pytorch.org"),
+]
+
+@pytest.mark.parametrize("URL, response", URL_test_success_data)
+def test_render_site_success(URL, response):
+    assert render_site(URL) == response
+
+@pytest.mark.parametrize("URL", URL_test_bad_data)
+def test_render_site_failed(URL):
+    with pytest.raises(InvalidURLException):
+        render_site(URL)
