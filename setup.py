1e05c46a5df80b591d1c03331aafb3b889c94582
c17hawke <sunny.c17hawke@gmail.com>
2022-09-06 21:11:51 +0530

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 ## edit below variables as per your requirements -
 REPO_NAME = "HTMLrenderer-c17hawke" # github repo name
